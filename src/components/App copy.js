24a0a12a4d147f9b3684848a99ebf96cbbc36317
Pablo Giroud <pablogiroud@gmail.com>
2024-04-29 18:47:54 -0300

    fix navbar

--- src/components/App copy.js
+++ src/components/App copy.js
@@ -2,18 +2,18 @@
 import "../styles/styles.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./Menu.js";
 import Footer from "./Footer.js";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Writing from "../pages/Writing";
 import Contact from "../pages/Contact";
-import NavBar from "../components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<NavBar />}>
+        <Route path="/" element={<Menu />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="writing" element={<Writing />} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { ReactComponent as Hamburger } from "../assets/icons/hamburguer.svg";
+import logo from "../../src/styles/images/logo.png";
+import "../styles/styles.css";
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="container">
+          <div className="logo">
+            <img alt="Pato's Logo" src={logo} />
+          </div>
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            <Hamburger />
+          </div>
+          <div className={`nav-elements  ${showNavbar && "active"}`}>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/writing">Writing</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Navbar;
