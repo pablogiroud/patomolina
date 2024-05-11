938598c51ded39ee0c45caa17b79be23b8711630
Pablo Giroud <pablogiroud@gmail.com>
2024-05-10 23:05:36 -0300

    add some cosmetics improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function Home() {
     let title = "Pato Molina"
-    let description = "Singer - Composer - Flutist - Writer - Orchestra director"
+    let description0 = "Singer - Composer"
+    let description1 = "Flutist - Writer"
+    let description2 = "Orchestra director"
     let image = ""
     let video = "/images/video.mp4"
 
@@ -13,19 +15,21 @@ function Home() {
                 <Helmet>
                     <meta name="author" content="Pato Molina" />
                     <title>{title}</title>
-                    <meta name="description" content={description} />
+                    <meta name="description" content="{{description0 + '-' + description1 + '-' + description2 }}" />
                     <meta property="og:title" content={title} />
-                    <meta property="og:description" content={description} />
+                    <meta property="og:description" content="{{description0 + '-' + description1 + '-' + description2 }}" />
                     <meta property="og:image" content={image} />
                     <meta property="og:type" content="website" />
                     <meta property="twitter:card" content="summary_large_image" />
                     <meta property="twitter:title" content={title} />
-                    <meta property="twitter:description" content={description} />
+                    <meta property="twitter:description" content="{{description0 + '-' + description1 + '-' + description2 }}" />
                     <meta name="viewport" content="width=device-width - initial-scale=1.0" />
                     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"></link>
                 </Helmet>
                 <h1>{title}</h1>
-                <h2>{description}</h2>
+                <h2>{description0}</h2>
+                <h2>{description1}</h2>
+                <h2>{description2}</h2>
                 <video autoPlay loop muted id="video">
                     <source src={video} type="video/mp4" />
                 </video>
