2c9c6ce9268e00ad16dcbc861f92de62ac570c25
Pablo Giroud <pablogiroud@gmail.com>
2024-05-22 01:06:52 -0300

    add captcha

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,17 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { Formik } from 'formik';
 import { successAlert, errorAlert } from '../utils/toastifyAlerts'
 import { ToastContainer } from 'react-toastify'
 import '../styles/form.css'
+import ReCAPTCHA from "react-google-recaptcha";
 
 function Contact() {
     let contact = 'Contact me';
+
+    const [captcha, setCaptcha] = useState();
+
     const handleSubmit = async (values) => {
-        console.log(process.env.REACT_APP_PUBLIC_KEY)
         try {
             await emailjs.send(
                 process.env.REACT_APP_SERVICE_ID,
@@ -33,12 +36,12 @@ function Contact() {
                     validate={values => {
                         const errors = {};
                         if (!values.email) {
-                            errors.email = 'Required';
+                            errors.email = 'Please, enter a valid email address';
                         } else if (
                             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                         ) {
                         } else if (!values.name) {
-                            errors.name = 'Required';
+                            errors.name = 'Pleas, enter a name';
                         }
                         return errors;
                     }}
@@ -53,7 +56,7 @@ function Contact() {
                         handleChange,
                         handleBlur,
                         handleSubmit,
-                        isSubmitting,
+                        isSubmitting
                     }) => (
                         <form className='form-login' onSubmit={handleSubmit}>
                             <input
@@ -96,10 +99,14 @@ function Contact() {
                                 value={values.message}
                             />
                             {errors.subject && touched.subject && errors.subject}
+                            <ReCAPTCHA
+                                sitekey={process.env.REACT_APP_CAPTCHA}
+                                onChange={(val) => setCaptcha(val)}
+                            />
                             <button
                                 className='btnSCV-long btnSCV-register'
                                 type="submit"
-                                disabled={isSubmitting}>
+                                disabled={isSubmitting || !captcha}>
                                 Submit
                             </button>
                         </form>
